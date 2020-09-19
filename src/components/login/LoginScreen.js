bbf47c631448637e61b9aa26a5f5c00346e24601
Jonathanleivag <email@jonathanleivag.com>
2020-09-19 19:26:16 -0300

    Login

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history: { replace } }) => {
     return (
-        <div>
-            <h1>LoginScreen</h1>
-        </div>
+        <section className='container mt-5'>
+            <h1>Login</h1>
+            <hr />
+            <button
+                className='btn btn-outline-success'
+                onClick={() => replace('/')}
+            >
+                Login
+            </button>
+        </section>
     )
 }
