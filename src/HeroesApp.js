be63fd42cdefce7dc9e4039d5ff918486fdc5857
Jonathanleivag <email@jonathanleivag.com>
2020-09-19 19:07:13 -0300

    Routers and screens

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,10 +1,6 @@
 import React from 'react'
+import { AppRouter } from './routers/AppRouter'
 
 export const HeroesApp = () => {
-  return (
-    <div className='container mt-5'>
-      <h1>Heroes App</h1>
-      <hr />
-    </div>
-  )
+  return <AppRouter />
 }
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+// componenst
+import { LoginScreen } from '../components/login/LoginScreen';
+import { DashboardRouter } from './DashboardRouter';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/login" component={LoginScreen} />
+                    <Route path="/" component={DashboardRouter} />
+                </Switch>
+            </div>
+        </Router>
+    )
+}
